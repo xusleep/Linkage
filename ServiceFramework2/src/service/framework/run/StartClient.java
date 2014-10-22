0a936826ad86bff1afdab648217d5defba8d10d1
Smile <Smile@Smile-VAIO>
2014-10-23 00:09:12 +0800

    线程达到10000个时候，更容易出现问题

--- ServiceFramework2/src/service/framework/run/StartClient.java
+++ ServiceFramework2/src/service/framework/run/StartClient.java
@@ -103,7 +103,7 @@ public class StartClient extends AbstractJob {
 		ClientBootStrap.getInstance().start();
     	ClientManagement cmm = new ClientManagement();
 		StartClient job1 = new StartClient((ConsumerBean)ClientBootStrap.getInstance().getApplicationContext().getBean("addService"));
-		job1.setThreadCount(1000);
+		job1.setThreadCount(10000);
 		List<JobInterface> jobList = new LinkedList<JobInterface>();
 		jobList.add(job1);
 		MainConcurrentThread mct1 = new MainConcurrentThread(jobList);
