bc0884559b7a7308e94a3ee08d81ee7928be76e5
zhonxu <xusleep@gmail.com>
2014-12-30 19:13:03 +0800

    refactoring

--- ServiceFramework2/java/main/service/middleware/framework/event/ServiceEvent.java
+++
@@ -1,6 +0,0 @@
-package service.middleware.framework.event;
-/*
- *  This is interface for all of the events
- */
-public interface ServiceEvent {
-}
--- ServiceFramework2/java/main/service/middleware/framework/handlers/Handler.java
+++
@@ -1,9 +0,0 @@
-package service.middleware.framework.handlers;
-
-import java.io.IOException;
-
-import service.middleware.framework.event.ServiceEvent;
-
-public interface Handler {
-	public void handleRequest(ServiceEvent event) throws IOException, Exception;
-}
