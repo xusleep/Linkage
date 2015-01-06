a37fd6d62944e084b4a59ee48a7b0e06910cad0f
zhonxu <xusleep@gmail.com>
2015-01-06 21:19:52 +0800

    comments

--- Linkage/java/main/service/middleware/linkage/framework/io/common/ReadWriteContext.java
+++ Linkage/java/main/service/middleware/linkage/framework/io/common/ReadWriteContext.java
@@ -1,5 +1,10 @@
 package service.middleware.linkage.framework.io.common;
 
+/**
+ * context pattern for the read write strategy
+ * @author zhonxu
+ *
+ */
 public interface ReadWriteContext extends ReadWriteStrategy {
 	
 }
