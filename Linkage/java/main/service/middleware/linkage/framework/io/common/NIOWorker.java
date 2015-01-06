9ec947632d7da9ce2b6885fb99e3794c0c3ecd46
zhonxu <xusleep@gmail.com>
2015-01-06 21:27:08 +0800

    update import comments

--- Linkage/java/main/service/middleware/linkage/framework/io/common/NIOWorker.java
+++ Linkage/java/main/service/middleware/linkage/framework/io/common/NIOWorker.java
@@ -79,6 +79,8 @@ public class NIOWorker implements Worker {
 						it.remove();
 						// 处理IO事件
 						if (key.isReadable()) {
+							// 这里不能使用 同步方法，
+							// 当使用同步方法时，如果传输大文件的话，会阻塞其他通道的数据
 							if(!read(key)){
 								this.closeChannel(key);
 							}
